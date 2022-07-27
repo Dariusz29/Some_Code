4ed83d5566d02f4131ea83123eb1f3b9e2ab8186
dariusz29 <dariusz.potepa29@gmail.com>
2022-07-27 23:32:16 +0200

    Add some code 0.2

--- dataframe_txt.py
+++ dataframe_txt.py
@@ -1,11 +1,19 @@
 
-
-
 t = open("words_tutlo.txt", "r")
+f = open('word.txt',"w")
+word_eng = []
+phases = []
+word_wrong = []
 
-
-print(t.read())
-
-
-
-t.close()
+for w in t:
+    if "=" in w:
+        data = w.split('=')
+        word_eng.append(data[0].lower())
+    elif "-" in w:
+        data_ = w.split('-')
+        word_eng.append(data_[0].lower())
+        
+for i in word_eng:
+    f.write(i)
+    f.write('\n')
+    
