8e518d55529c38e7af132490b3c4ec81fb087ac2
dariusz29 <dariusz.potepa29@gmail.com>
2022-07-28 22:03:23 +0200

    Add new modified in my code

--- dataframe_txt.py
+++ dataframe_txt.py
@@ -1,39 +1,58 @@
 
-t = open("words_tutlo.txt", "r")
-f = open('word.txt',"w")
-g = open('translated_word.txt','w')
-h = open('translated_phases.txt','w')
+word_sourse = open("words_tutlo.txt", "r")
+word_sourse_ = open("new_words.txt", "r")
+phases_sourse = open("new_phases.txt", "r")
+word_in_eng = open('word_in_eng.txt',"w")
+word_answer = open('word_answer.txt','w')
+phases_in_eng = open('phases_in_eng.txt','w')
+phases_answer = open('phases_answer.txt', 'w')
 word_eng = []
-phases = []
-word_wrong = []
+word_ans = []
+phases_eng = []
+phases_ans = []
 
 
-for w in t:
+for w in word_sourse, word_sourse_:
     if "=" in w:
         data = w.split('=')
         word_eng.append(data[0].lower())
-        phases.append(data[1].lower())
+        word_ans.append(data[1].lower())
     elif "-" in w:
         data = w.split('-')
         word_eng.append(data[0].lower())
-        phases.append(data[1].lower())
+        word_ans.append(data[1].lower())
     
-        
-for i in word_eng:
-    if len(i) < 20: 
-        f.write(i)
-        f.write('\n')
-    else:
-        h.write(i)
-        h.write('\n')
+for w_ in phases_sourse:
+    if "=" in w:
+        data = w.split('=')
+        phases_eng.append(data[0].lower())
+        phases_ans.append(data[1].lower())
+    elif "-" in w:
+        data = w.split('-')
+        phases_eng.append(data[0].lower())
+        phases_ans.append(data[1].lower())
 
+for i in word_eng, word_ans:
+    if word_eng == i:  
+        word_in_eng.write(i)
+        word_in_eng.write('\n')
+    elif word_ans:
+        word_answer.write(i)
+        word_answer.write('\n')
+   
 
+for i in phases_eng, phases_ans:
+    if phases_eng:
+        phases_in_eng.write(i)
+        phases_in_eng.write('\n')
+    elif phases_ans:
+        phases_answer.write(i)
+        phases_answer.write('\n')
 
-
-for i in phases:
-    g.write(i)
     
 
     
-t.close()
-f.close()
+word_in_eng.close()
+word_answer.close()
+phases_in_eng.close()
+phases_answer.close()
--- notes.py
+++ notes.py
@@ -1,5 +1,14 @@
 # It is a test
 
-      
+a  = ['a','b','c','d']
+b = ['q','w']
+
+
+for i in a, b:
+    if i == a:
+        print('1',i[0:4])
+    elif i == b:
+        print('2',i)
+
 
 
