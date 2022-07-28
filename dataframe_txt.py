767c174779f9c9e888e74bedf59a3b2fc49fee45
dariusz29 <dariusz.potepa29@gmail.com>
2022-07-28 18:38:34 +0200

    Add new modified in my code

--- dataframe_txt.py
+++ dataframe_txt.py
@@ -1,6 +1,8 @@
 
 t = open("words_tutlo.txt", "r")
 f = open('word.txt',"w")
+g = open('translated_word.txt','w')
+h = 
 word_eng = []
 phases = []
 word_wrong = []
@@ -9,11 +11,26 @@ for w in t:
     if "=" in w:
         data = w.split('=')
         word_eng.append(data[0].lower())
+        phases.append(data[1].lower())
     elif "-" in w:
-        data_ = w.split('-')
-        word_eng.append(data_[0].lower())
+        data = w.split('-')
+        word_eng.append(data[0].lower())
+        phases.append(data[1].lower())
+    
         
 for i in word_eng:
-    f.write(i)
-    f.write('\n')
+    if len(i) < 20: 
+        f.write(i)
+        f.write('\n')
+    else:
+
+
+
+
+for i in phases:
+    g.write(i)
     
+
+    
+t.close()
+f.close()
