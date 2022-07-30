db16f7c78522f34d6205b0b305c24c4af6cc754c
dariusz29 <dariusz.potepa29@gmail.com>
2022-07-30 15:25:17 +0200

    clean my data and my messy in dictionaries

--- notes.py
+++ notes.py
@@ -1,14 +1,13 @@
 # It is a test
 
-a  = ['a','b','c','d']
-b = ['q','w']
+# a  = ['a','b','c','d']
+# b = ['q','w']
 
 
-for i in a, b:
-    if i == a:
-        print('1',i[0:4])
-    elif i == b:
-        print('2',i)
-
+# for i in a, b:
+#     if i == a:
+#         print('1',i[0:4])
+#     elif i == b:
+#         print('2',i)
 
 
