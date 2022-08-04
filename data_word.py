6c102eb1ad982202120aad348d048de3cf62de78
dariusz29 <dariusz.potepa29@gmail.com>
2022-08-04 19:41:38 +0200

    Start type code to do test

---
+++ data_word.py
@@ -0,0 +1,73 @@
+
+word_sourse = open("words_tutlo.txt", "r")
+word_sourse_ = open("new_words.txt", "r")
+phases_sourse = open("new_phases.txt", "r")
+word_in_eng = open('word_in_eng.txt',"w")
+word_answer = open('word_answer.txt','w')
+phases_in_eng = open('phases_in_eng.txt','w')
+phases_answer = open('phases_answer.txt', 'w')
+
+
+word_eng = []
+word_ans = []
+phases_eng = []
+phases_ans = []
+
+def store_word():
+
+    for w in word_sourse:
+
+        if "=" in w:
+            data = w.split('=')
+            word_eng.append(data[0].lower())
+            word_ans.append(data[1].lower())
+        elif "-" in w:
+            data = w.split('-')
+            word_eng.append(data[0].lower())
+            word_ans.append(data[1].lower())
+
+    for w in word_sourse_:
+
+        if "=" in w:
+            data = w.split('=')
+            word_eng.append(data[0].lower())
+            word_ans.append(data[1].lower())
+        elif "-" in w:
+            data = w.split('-')
+            word_eng.append(data[0].lower())
+            word_ans.append(data[1].lower())
+        
+    for w in phases_sourse:
+        if "=" in w :
+            data = w.split('=')
+            phases_eng.append(data[0].lower())
+            phases_ans.append(data[1].lower())
+        elif "-" in w :
+            data = w.split('-')
+            phases_eng.append(data[0].lower())
+            phases_ans.append(data[1].lower())
+
+    for i in word_eng:
+        word_in_eng.write(i)
+        word_in_eng.write('\n')
+
+    for i in word_ans:
+        word_answer.write(i)
+        
+
+    for i in phases_eng: 
+        phases_in_eng.write(i)
+        phases_in_eng.write('\n')
+
+    for i in phases_ans:
+        phases_answer.write(i)
+        
+            
+        
+
+        
+    word_in_eng.close()
+    word_answer.close()
+    phases_in_eng.close()
+    phases_answer.close()
+
--- notes.py
+++ notes.py
@@ -10,8 +10,15 @@
 #     elif i == b:
 #         print('2',i)
 
-c=[]
-a = [1,2,3,4,5]
-b = [1,4,6,7,8,9]
-for i in a,b:
-    c.append(i)
+# c=[]
+# a = [1,2,3,4,5]
+# b = [1,4,6,7,8,9]
+# for i in a,b:
+#     c.append(i)
+from math import sqrt
+a = 0.508
+b = 0.312
+
+x = (a*2)**2 + (b*2)**2
+
+print(sqrt(x)/2.54)
