ac5a7e8ca2e3ffbf5c97bd07e2a6cc8a590a6265
dariusz29 <dariusz.potepa29@gmail.com>
2022-08-04 21:04:57 +0200

    Modified code

--- data_word.py
+++ data_word.py
@@ -13,61 +13,79 @@ word_ans = []
 phases_eng = []
 phases_ans = []
 
-def store_word():
+def store_word(resp):
 
-    for w in word_sourse:
+   
+    def wordss():
+    
+        for w in word_sourse:
 
-        if "=" in w:
-            data = w.split('=')
-            word_eng.append(data[0].lower())
-            word_ans.append(data[1].lower())
-        elif "-" in w:
-            data = w.split('-')
-            word_eng.append(data[0].lower())
-            word_ans.append(data[1].lower())
+            if "=" in w:
+                data = w.split('=')
+                word_eng.append(data[0].lower())
+                word_ans.append(data[1].lower())
+            elif "-" in w:
+                data = w.split('-')
+                word_eng.append(data[0].lower())
+                word_ans.append(data[1].lower())
 
-    for w in word_sourse_:
+        for w in word_sourse_:
 
-        if "=" in w:
-            data = w.split('=')
-            word_eng.append(data[0].lower())
-            word_ans.append(data[1].lower())
-        elif "-" in w:
-            data = w.split('-')
-            word_eng.append(data[0].lower())
-            word_ans.append(data[1].lower())
-        
-    for w in phases_sourse:
-        if "=" in w :
-            data = w.split('=')
-            phases_eng.append(data[0].lower())
-            phases_ans.append(data[1].lower())
-        elif "-" in w :
-            data = w.split('-')
-            phases_eng.append(data[0].lower())
-            phases_ans.append(data[1].lower())
+            if "=" in w:
+                data = w.split('=')
+                word_eng.append(data[0].lower())
+                word_ans.append(data[1].lower())
+            elif "-" in w:
+                data = w.split('-')
+                word_eng.append(data[0].lower())
+                word_ans.append(data[1].lower())
 
-    for i in word_eng:
-        word_in_eng.write(i)
-        word_in_eng.write('\n')
+        for i in word_eng:
+            word_in_eng.write(i)
+            word_in_eng.write('\n')
 
-    for i in word_ans:
-        word_answer.write(i)
-        
+        for i in word_ans:
+            word_answer.write(i)
 
-    for i in phases_eng: 
-        phases_in_eng.write(i)
-        phases_in_eng.write('\n')
 
-    for i in phases_ans:
-        phases_answer.write(i)
+    def phass():  
+      
+        for w in phases_sourse:
+
+            if "=" in w :
+                data = w.split('=')
+                phases_eng.append(data[0].lower())
+                phases_ans.append(data[1].lower())
+            elif "-" in w :
+                data = w.split('-')
+                phases_eng.append(data[0].lower())
+                phases_ans.append(data[1].lower())
+
+        for i in phases_eng: 
+            phases_in_eng.write(i)
+            phases_in_eng.write('\n')
+
+        for i in phases_ans:
+            phases_answer.write(i)
+
+
+    if resp == 'w':
+        return wordss()
+    elif resp == 'p':
+        return phass()
+
+
+    
+
+
         
             
         
 
         
-    word_in_eng.close()
-    word_answer.close()
-    phases_in_eng.close()
-    phases_answer.close()
+word_in_eng.close()
+word_answer.close()
+phases_in_eng.close()
+phases_answer.close()
+
 
